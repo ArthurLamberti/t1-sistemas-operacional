6ffba0d8cb7b4cde0bbc04e9eed7b4cc51a66252
Arthur Lamberti <arthurlamberti001@gmail.com>
2021-04-26 01:58:37 +0000

    correcao pra usar o jpa

--- src/main/java/com/bcopstein/CtrlCorredorV1/aplicacao/servicos/EstatisticaNormal.java
+++ src/main/java/com/bcopstein/CtrlCorredorV1/aplicacao/servicos/EstatisticaNormal.java
@@ -2,16 +2,23 @@ package com.bcopstein.CtrlCorredorV1.aplicacao.servicos;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import com.bcopstein.CtrlCorredorV1.aplicacao.dtos.EstatisticasDTO;
 import com.bcopstein.CtrlCorredorV1.negocio.entidades.Evento;
+import com.bcopstein.CtrlCorredorV1.negocio.entidades.EventoJpa;
 import com.bcopstein.CtrlCorredorV1.negocio.repositorios.IEventoRepository;
+import com.bcopstein.CtrlCorredorV1.negocio.repositorios.IEventoRepositoryJpa;
 import com.bcopstein.CtrlCorredorV1.negocio.util.EventoUtils;
+import com.bcopstein.CtrlCorredorV1.negocio.util.EventoUtilsJpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class EstatisticaNormal implements ICalculoEstatistica {
     private IEventoRepository eventoRep;
+
+    @Autowired
+    private IEventoRepositoryJpa eventoRepository;
     
     @Autowired
     public EstatisticaNormal(IEventoRepository eventoRep){
@@ -20,10 +27,12 @@ public class EstatisticaNormal implements ICalculoEstatistica {
 
     public EstatisticasDTO calculaEstatisticas(int distancia){
         
-        List<Evento> eventos = eventoRep.getEventosByDistancia(distancia);
-        Double media = EventoUtils.calcularMedia(eventos);
-        Double mediana = EventoUtils.calcularMediana(eventos);
-        Double desvioPadrao = EventoUtils.calcularDesvio(eventos, media);
+        Iterable<EventoJpa> eventosIterable = eventoRepository.findByDistancia(distancia);
+        System.out.println("Buscou");
+        List<EventoJpa> eventos = StreamSupport.stream(eventosIterable.spliterator(), false).collect(Collectors.toList());
+        Double media = EventoUtilsJpa.calcularMedia(eventos);
+        Double mediana = EventoUtilsJpa.calcularMediana(eventos);
+        Double desvioPadrao = EventoUtilsJpa.calcularDesvio(eventos, media);
         if(!eventos.isEmpty()){
             return new EstatisticasDTO(media, mediana, desvioPadrao);
         }
